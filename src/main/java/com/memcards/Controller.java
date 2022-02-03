f8d74778a052536f79140452b06ac06be9dde19f
kit <artemjevnikita@yandex.ru>
2022-02-03 18:41:14 +0400

    Added "Clear list words" to the menu Added a functionality for the menuOpenFile

--- src/main/java/com/memcards/Controller.java
+++ src/main/java/com/memcards/Controller.java
@@ -4,16 +4,24 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import java.nio.file.Path;
 
 public class Controller {
+    FileChooser fileChooser = new FileChooser();
+
+    @FXML
+    private MenuItem clearListItem;
 
     @FXML
     private Button forgotButton;
 
     @FXML
-    private ListView<?> listView;
+    private ListView<String> listView;
 
     @FXML
     private ProgressBar progressBar;
@@ -28,13 +36,20 @@ public class Controller {
     private Text wordText;
 
     @FXML
+    void clearListItemAction(ActionEvent event) {
+        listView.getItems().clear();
+    }
+
+    @FXML
     void forgotButtonAction(ActionEvent event) {
 
     }
 
     @FXML
     void menuOpenFile(ActionEvent event) {
-
+        Path path = fileChooser.showOpenDialog(new Stage()).toPath();
+        ExcelParse excelParse = new ExcelParse(path);
+        listView.getItems().addAll(excelParse.getItems().keySet());
     }
 
     @FXML
