064e1cc4fe78538fd13536f72c4aececa1ddb89b
kit <artemjevnikita@yandex.ru>
2022-02-03 14:23:22 +0400

    Added elements id

--- src/main/java/com/memcards/Controller.java
+++ src/main/java/com/memcards/Controller.java
@@ -1,11 +1,45 @@
 package com.memcards;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.text.Text;
 
 public class Controller {
 
     @FXML
-    private ListView<String> listView;
+    private Button forgotButton;
+
+    @FXML
+    private ListView<?> listView;
+
+    @FXML
+    private ProgressBar progressBar;
+
+    @FXML
+    private Button rememberButton;
+
+    @FXML
+    private Text translateText;
+
+    @FXML
+    private Text wordText;
+
+    @FXML
+    void forgotButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void menuOpenFile(ActionEvent event) {
+
+    }
+
+    @FXML
+    void rememberButtonAction(ActionEvent event) {
+
+    }
 
 }
